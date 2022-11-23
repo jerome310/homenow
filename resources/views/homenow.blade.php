4183a24d9c4ea738adc59e5208355a04e9ed2178
jerome310 <jeromeyoung333@gmail.com>
2022-11-23 14:05:21 -0500

    Tailwind desktop version completed

--- resources/views/homenow.blade.php
+++ resources/views/homenow.blade.php
@@ -9,20 +9,20 @@
     <title>Document</title>
 </head>
 
-<body style="background: url('https://i.ibb.co/5RcpBct/homenow-background-image.jpg') no-repeat;"
-    class="bg-fixed text-[#ffffff] w-[100%] h-[125vh] relative bg-cover bg-center">
-    <div class="flex flex-col items-center justify-center">
-        <div class="main-text">
-            <a class="logo" href="https://ibb.co/9YND1Gj"><img src="https://i.ibb.co/F7Krynt/homenow-logo.png"
+<body style="background: url('https://i.ibb.co/5RcpBct/homenow-background-image.jpg') no-repeat; background-size: cover; background-attachment:fixed; background-position: center;"
+    class="text-[#ffffff] w-[100%] h-[125vh] relative">
+    <div class="flex flex-col items-center justify-center h-[40vh] absolute z-[1] p-[45px] w-[100%]">
+        <div class="font-[Roboto, sans-serif]">
+            <a class="pt[-50%] w-[150px] mr-auto" href="https://ibb.co/9YND1Gj"><img class="w-[130px] mt-[50%] pt[150px]" src="https://i.ibb.co/F7Krynt/homenow-logo.png"
                     alt="homenow-logo" border="0"></a>
-            <p>BE THE FIRST TO KNOW</p>
-            <h1>Searching for a new home in the Hampton neighborhood?</h1>
-            <p class="text-description">Get updates BEFORE a new home is even listed!</p>
-            <a class="button" href="/">Send me updates first &#8702;</a>
+            <p class="pt-[100px] pl-[5px] text-[16px] w-[100%]">BE THE FIRST TO KNOW</p>
+            <h1 class="pt-[15px] pb-[5px] text-[40px] font-[700] mr-[auto]">Searching for a new home in the Hampton neighborhood?</h1>
+            <p class="pt-[30px] pb-[50px] text-[20px] w-[43%] mr-[auto]">Get updates BEFORE a new home is even listed!</p>
+            <a class="mr-[auto] bg-[#E70F2B] py-[25px] px-[50px] text-[20px] text-[#ffffff]" href="/">Send me updates first &#8702;</a>
         </div>
-        <footer>
+        <footer class="pt-[20%] w-[80%] flex justify-between items-center text-[14px]">
             <div>
-                <a href="/">Contact Us</a>
+                <a class="underline" href="/">Contact Us</a>
             </div>
             <div>
                 <span>© 2020 HomeNow, All Rights Reserved. Created with</span>
@@ -33,7 +33,7 @@
             </div>
         </footer>
     </div>
-    <div class="color-overlay"></div>
+    <div class="w-[100%] h-[100%] bg-[#000942b3] opacity-[.7] absolute"></div>
 </body>
 
 </html>
