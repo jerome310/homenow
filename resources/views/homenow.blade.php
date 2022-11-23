149f94a8e6fc943da0db67b9eb262f8897a8612e
jerome310 <jeromeyoung333@gmail.com>
2022-11-23 14:34:38 -0500

    Completed mobile version of homenow

--- resources/views/homenow.blade.php
+++ resources/views/homenow.blade.php
@@ -13,14 +13,14 @@
     class="text-[#ffffff] w-[100%] h-[125vh] relative">
     <div class="flex flex-col items-center justify-center h-[40vh] absolute z-[1] p-[45px] w-[100%]">
         <div class="font-[Roboto, sans-serif]">
-            <a class="pt[-50%] w-[150px] mr-auto" href="https://ibb.co/9YND1Gj"><img class="w-[130px] mt-[50%] pt[150px]" src="https://i.ibb.co/F7Krynt/homenow-logo.png"
+            <a class="mobile:pt-[120%] table:pt-[50%] w-[150px] mr-auto" href="https://ibb.co/9YND1Gj"><img class="mobile:mt-[150%] w-[130px] tablet:mt-[50%] pt[150px]" src="https://i.ibb.co/F7Krynt/homenow-logo.png"
                     alt="homenow-logo" border="0"></a>
             <p class="pt-[100px] pl-[5px] text-[16px] w-[100%]">BE THE FIRST TO KNOW</p>
-            <h1 class="pt-[15px] pb-[5px] text-[40px] font-[700] mr-[auto]">Searching for a new home in the Hampton neighborhood?</h1>
-            <p class="pt-[30px] pb-[50px] text-[20px] w-[43%] mr-[auto]">Get updates BEFORE a new home is even listed!</p>
+            <h1 class="tablet:font-[700] pt-[15px] pb-[5px] text-[40px] mobile:font-[900] mr-[auto]">Searching for a new home in the Hampton neighborhood?</h1>
+            <p class="pt-[30px] pb-[60px] text-[20px] w-[90%] mr-[auto]">Get updates BEFORE a new home is even listed!</p>
             <a class="mr-[auto] bg-[#E70F2B] py-[25px] px-[50px] text-[20px] text-[#ffffff]" href="/">Send me updates first &#8702;</a>
         </div>
-        <footer class="pt-[20%] w-[80%] flex justify-between items-center text-[14px]">
+        <footer class="leading-[4em] m-[0 auto] pt-[20%] w-[100%] flex flex-col justify-center items-center text-[12px]">
             <div>
                 <a class="underline" href="/">Contact Us</a>
             </div>
